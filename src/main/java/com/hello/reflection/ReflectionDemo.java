1c9f9687d480d7e586c002078aa9c0406f5bd869
Eswara Poojari <eswara.poojari@gmail.com>
2018-12-26 23:06:32 +0530

    addedd reflection class

---
+++ src/main/java/com/hello/reflection/ReflectionDemo.java
@@ -0,0 +1,31 @@
+package com.hello.reflection;
+
+public class ReflectionDemo {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// using class name
+		String name = Employee.class.getName();
+		//using object ref
+		Class<? extends Employee> class1 = new Employee().getClass();
+		// using forName()
+		Class<?> forName = Class.forName("com.hello.reflection.Employee");
+
+	}
+
+}
+
+class Employee{
+	private int id;
+	String name;
+	protected int salary;
+	public int age;
+	
+	
+	Employee(){}
+	Employee(int id, String name){}
+	
+	public void m1(){}
+	private void m2(){}
+	protected void m3(){}
+	void m4(){}
+}
