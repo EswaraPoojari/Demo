9ce739fbd402549d9489ec9313114e8ce4594301
Eswara Poojari <eswara.poojari@gmail.com>
2018-12-29 15:12:17 +0530

    Modified HelloWorld From eclipse

--- src/main/java/com/hello/HelloWorld.java
+++ src/main/java/com/hello/HelloWorld.java
@@ -3,7 +3,8 @@ package com.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welecome to Java World");
+		System.out.println("Hello World");
+		
 	}
 
 }
